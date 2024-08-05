6327f77ffcf8564e5b38b17c533e144a116cd2ca
Mikias Derseh <mikiyasbaile@gmail.com>
2024-08-06 01:20:25 +0300

    first commit

---
+++ lib/button_values.dart
@@ -0,0 +1,44 @@
+class Btn {
+  static const String delete = "D";
+  static const String clear = "C";
+  static const String percentage = "%";
+  static const String multiply = "×";
+  static const String divide = "÷";
+  static const String add = "+";
+  static const String subtract = "-";
+  static const String calculate = "=";
+  static const String dot = ".";
+
+  static const String num0 = "0";
+  static const String num1 = "1";
+  static const String num2 = "2";
+  static const String num3 = "3";
+  static const String num4 = "4";
+  static const String num5 = "5";
+  static const String num6 = "6";
+  static const String num7 = "7";
+  static const String num8 = "8";
+  static const String num9 = "9";
+
+  static const List<String> buttonValues = [
+    delete,
+    clear,
+    percentage,
+    multiply,
+    num7,
+    num8,
+    num9,
+    divide,
+    num4,
+    num5,
+    num6,
+    subtract,
+    num1,
+    num2,
+    num3,
+    add,
+    num0,
+    dot,
+    calculate,
+  ];
+}
